d3a42a842e4b60b92e2db8813b861642ebb04d46
datascientette <risa.kawai@gmail.com>
2013-09-01 17:41:25 +0000

    First YQL query

--- Cookies/Cookies/Program.cs
+++ Cookies/Cookies/Program.cs
@@ -10,6 +10,7 @@ namespace Cookies
     {
         static void Main(string[] args)
         {
+            YQL testYQL = new YQL("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fallrecipes.com%2Frecipe%2Fcookie-jar-sugar-cookies%2F%22%20and%20compat%3D%22html5%22%20AND%20xpath%20%3D%20%22%2F%2F%40data-ingredientid%22&diagnostics=true");
         }
     }
 }
---
+++ Cookies/Cookies/YQL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+
+namespace Cookies
+{
+    class YQL
+    {
+        string restQuery;
+        XDocument website;
+
+        public YQL(string query)
+        {
+            //Change this to parse query, for now, assume already formated as rest Query
+            this.restQuery = query;
+            this.website = XDocument.Load(restQuery);
+            Debug.Write(website);
+        }
+
+    }
+}
